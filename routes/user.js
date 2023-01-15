5e00b4c62a1aed6d762cfdedbe98f43c217f80d3
Kristina Djurovic <3435.kristina@gmail.com>
2023-01-15 17:12:07 +0100

    parametri

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../helpers/database');
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken')
 
 router.get('/:id', async function(req,res){
+    
     try {
-        const sqlQuery = 'SELECT id, email, password, created_at FROM user WHERE id=?';
+        const sqlQuery = 'SELECT id, email, password, created_at, user_type FROM user WHERE id=?';
         const rows = await pool.query(sqlQuery, req.params.id);
         res.status(200).json(rows);
     } catch (error) {
@@ -19,12 +20,12 @@ router.get('/:id', async function(req,res){
 
 router.post('/register', async function(req,res) {
     try {
-        const {email, password, type} = req.body;
+        const {email, password, user_type} = req.body;
         
         const encryptedPassword = await bcrypt.hash(password,10)
 
-        const sqlQuery = 'INSERT INTO user (email, password,type) VALUES (?,?,?)';
-        const result = await pool.query(sqlQuery, [email, encryptedPassword, type]);
+        const sqlQuery = 'INSERT INTO user (email, password,user_type) VALUES (?,?,?)';
+        const result = await pool.query(sqlQuery, [email, encryptedPassword, user_type]);
 
         res.status(200).json("Poslato");
     } catch (error) {
@@ -36,17 +37,20 @@ router.post('/login', async function(req,res) {
     try {
         const {email,password} = req.body;
 
-        const sqlGetUser = 'SELECT password FROM user WHERE email=?';
+        const sqlGetUser = 'SELECT password, id, user_type FROM user WHERE email=?';
         const rows = await pool.query(sqlGetUser,email);
         if(rows){
-            
+            id = rows[0].id;
+            user_type = rows[0].user_type;
             const isValid = await bcrypt.compare(password,rows[0].password)
-            res.status(200).json({valid_password: isValid});
+            const token = jwt.sign({id,user_type},'Kristina voli Nikolu <3');
+                
+            res.status(200).json({token: token, id: rows[0].id, user_type: user_type});
         }
         res.status(200).send(`User with email ${email} was not found`);
         
     } catch (error) {
-       // res.status(400).send(error.message)
+        res.status(400).send(error.message)
     }
 })
 
